e8e931862ed7d2b205927a2eb38f8173388e9e6d
Ali Asadi <4.aliasad.4@gmail.com>
2019-01-02 00:00:11 +0330

    first commit

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,20 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args){
-
+        Scanner input = new Scanner(System.in);
+        int size = Integer.parseInt((input.nextLine()));
+        String[][] rule = new String[size][2];
+        String[] R = new String[2];
+        for (int i = 0; i < size; i++){
+            String r = input.nextLine();
+            R = r.split(" -> ");
+            rule[i][0] = R[0];
+            rule[i][1] = R[1];
+        }
+        for (int i = 0; i < size; i++){
+            System.out.print(rule[i][0]);
+            System.out.print(rule[i][1]);
+        }
     }
 }
