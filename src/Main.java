d8ea9d7e0a466e0bf5f1e99cd4fd67e664248fb6
Ali Asadi <4.aliasad.4@gmail.com>
2019-01-02 21:19:29 +0330

    first commit

--- src/Main.java
+++ src/Main.java
@@ -12,9 +12,47 @@ public class Main {
             rule[i][0] = R[0];
             rule[i][1] = R[1];
         }
-        for (int i = 0; i < size; i++){
-            System.out.print(rule[i][0]);
-            System.out.print(rule[i][1]);
+        String[] c = new String[0];
+        int[] d = new int[rule.length];
+        for (int i = 0 ; i < rule.length; i++) {
+            if (rule[i][1].length() > 1) {
+                c = new String[rule[i][1].length()];
+                for (int j = 0; j < rule[i][1].length(); j++) {
+                    c = (rule[i][1].split(""));
+                }
+            }
         }
+
+        for (int i = 0; i < size; i++){
+            System.out.print(c[i]);
+            System.out.print(c[i]);
+            System.out.println();
+        }
+// for (int i = 0; i < size; i++){
+//            System.out.print(rule[i][0]);
+//            System.out.print(rule[i][1]);
+//            System.out.println();
+//        }
+//        if (testgrammer(rule)){
+//            System.out.println("Wrong Grammar");
+//        }
+//        String[] c = new String[size];
+//        for (int i = 0; i < rule.length; i++){
+//            c[i] = String.valueOf(rule[i][1].split(""));
+//        }
+
     }
+//    public static Boolean testgrammer(String[][] rule) {
+//        for (int i = 0 ; i < rule.length; i++){
+//            if (rule[i][1].length() > 1){
+//                char[] c = new char[rule.length];
+//                for(int j = 0; i < rule.length; i++){
+//                     c[j] += (rule[j][1].split("")).toString().charAt(0);
+//                }
+//                return false;
+//            }
+//        }
+//
+//        return true;
+//    }
 }
