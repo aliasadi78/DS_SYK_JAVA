0115b1fff1fa7c08d4735d370c85a8cec88b4886
Ali Asadi <4.aliasad.4@gmail.com>
2019-01-04 22:41:11 +0330

    first commit

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,4 @@
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Main {
@@ -12,11 +13,44 @@ public class Main {
             rule[i][0] = R[0];
             rule[i][1] = R[1];
         }
-        String vor = input.nextLine();
+        String vor = input.nextLine().toUpperCase();
+        ArrayList v = new ArrayList(vor.length());
+        String[] v1 = new String[vor.length()];
+        v1 = vor.split("");
+        for (int i = 0; i < vor.length(); i++){
+            v.add(i,v1[i]);
+        }
         if (!testgrammer(rule)){
             System.out.println("Wrong Grammar");
         }
+        else {
+            System.out.println("NO");
+        }
+//        else (cyk(rule,v)){
+//            System.out.println("NO");
+//        }
     }
+
+    public static Boolean cyk(String[][] rule,ArrayList v){
+        Boolean T = true;
+        String[][] str = new String[rule.length][2];
+        String[] s = new String[rule.length];
+
+        for (int i = 0; i < v.size(); i++){
+            s = rule[i][1].toUpperCase().split("");
+            for (int j = 0; j < rule.length; j++){
+                if (v.get(i).equals(s[0])){
+                    str[i][0] = rule[i][0];
+                    System.out.println(str[i][i]);
+                }
+
+            }
+
+
+        }
+        return true;
+    }
+
     public static Boolean testgrammer(String[][] rule) {
         String[] c;
         Boolean T = true;
@@ -56,3 +90,32 @@ public class Main {
         return T;
     }
 }
+//import java.util.ArrayList;
+//
+//public class Main{
+//    public static void main(String[] args){
+//        String str = "aabbb";
+//        int n = str.length() * (str.length() + 1 ) / 2;
+//        ArrayList str1 = new ArrayList(n);
+//        for (int i = 0; i <= str.length(); i++){
+//            for (int j = i; j <= str.length(); j++){
+//                if (i!=j) {
+//                    str1.add(str.substring(i, j));
+//
+//
+//                }
+//            }
+//        }
+//        System.out.println(str1);
+//        ArrayList Str = new ArrayList(n);
+//        for (int i = 1; i <= str.length(); i++) {
+//            int j = 0;
+//            for (j = 0; j < n; j++) {
+//                if (str1.get(j).toString().length() == i){
+//                    Str.add(str1.get(j));
+//                }
+//            }
+//        }
+//        System.out.println(Str);
+//    }
+//}
