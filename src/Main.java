c56475e53ec1b6b6eb5104d06466e626ab61b675
Ali Asadi <4.aliasad.4@gmail.com>
2019-01-08 01:15:02 +0330

    first commit

--- src/Main.java
+++ src/Main.java
@@ -4,46 +4,41 @@ import java.util.Arrays;
 import java.util.Scanner;
 
 public class Main {
-    public static final int MAX = 100;
+    public static final int MAX = 1700;
     public static String[][] gram = new String[MAX][MAX];
     public static String[] dpr = new String[MAX];
     public static String[][] matrix = new String[MAX][MAX];
     public static String[][] matrix1 = new String[MAX][MAX];
     public static int np;
-    //    public static String[][] rulee = new String[np][2];
-    public static String str;
+    public static String start;
     public static String rrule;
     public static String[] R = new String[2];
+    public static String str;
     public static int p = 0;
 
 
     public static void main(String[] args) {
-        for (int y = 0; y < MAX; y++){
-            for (int x = 0; x < MAX; x++){
+        for (int y = 0; y < MAX; y++) {
+            for (int x = 0; x < MAX; x++) {
                 gram[y][x] = "";
-
             }
         }
-        for (int y = 0; y < MAX; y++){
+        for (int y = 0; y < MAX; y++) {
             dpr[y] = "";
         }
         int i;
         int j;
         int l;
         int k;
-        String str;
         String r;
         String pr;
-        String start;
 
-        final int MAX = 100;
         Scanner input = new Scanner(System.in);
         start = "S";
         np = Integer.parseInt(input.nextLine());
         String[][] rul = new String[np][2];
-        for (i = 0; i < np; i++)
-        {
-            for (int b = 0; i < np; i++){
+        for (i = 0; i < np; i++) {
+            for (int b = 0; i < np; i++) {
                 rrule = input.nextLine();
                 R = rrule.split(" -> ");
                 gram[i][0] = R[0];
@@ -51,22 +46,18 @@ public class Main {
                 rul[i][0] = R[0];
                 rul[i][1] = R[1];
             }
-
-            for (j = 0; j < p; j++)
-            {
+            for (j = 0; j < p; j++) {
                 gram[i][j + 1] = dpr[j];
             }
-
         }
-        for (int y = 0; y < MAX; y++){
-            for (int x = 0; x < MAX; x++){
+        for (int y = 0; y < MAX; y++) {
+            for (int x = 0; x < MAX; x++) {
                 matrix[y][x] = "";
                 matrix1[y][x] = "";
             }
         }
         String st;
         str = input.nextLine();
-
 
         for (i = 0; i < str.length(); i++) {
             r = "";
@@ -81,10 +72,7 @@ public class Main {
                     k++;
                 }
             }
-            ArrayList F = sortt(r);
-            String rr = F.toString();
-            rr.replace(" ", "");
-            F.clear();
+            String rr = sortt(r);
             matrix[i][i] = rr;
         }
         for (k = 1; k < str.length(); k++) {
@@ -94,9 +82,7 @@ public class Main {
                     pr = gen_comb(matrix[j - k][l], matrix[l + 1][j]);
                     r = concat(r, pr);
                 }
-                ArrayList E = sortt(r);
-                String rr = E.toString();
-                E.clear();
+                String rr = sortt(r);
                 matrix[j - k][j] = rr;
             }
         }
@@ -114,31 +100,43 @@ public class Main {
                 matrix1[i][j] = str1.get(mm).toString();
                 mm++;
             }
-        }
-        if (!testgrammer(rul)){
+        }Boolean I = false;
+        if (! testgrammer(rul)) {
             System.out.println("Wrong Grammar");
         }
-        else {
-            if (matrix[0][str.length() - 1].equals("")) {
-                System.out.println("NO");
-            } else {
-                System.out.println("YES");
-                int mn;
-                for (i = 0; i < str.length(); i++) {
-                    mn = 0;
-                    for (j = i; j < str.length(); j++) {
-                        System.out.print(matrix1[mn][j].replace(" ", ""));
-                        System.out.print(" : ");
-                        System.out.print(matrix[mn++][j]);
-                        if (j != str.length() - 1) {
-                            System.out.print(" , ");
+        else{
+            ArrayList T = new ArrayList();
+            String[] t = new String[(matrix[0][str.length() - 1]).length()];
+            t = (matrix[0][str.length() - 1]).split("");
+            for (i = 0; i < t.length; i++){
+                T.add(t[i]);
+            }
+            if (T.size() > 0){
+                for (i = 0; i < T.size(); i++){
+                    if (T.get(i).equals("S")) {
+                        System.out.println("YES");
+                        int mn;
+                        I = true;
+                        for (i = 0; i < str.length(); i++) {
+                            mn = 0;
+                            for (j = i; j < str.length(); j++) {
+                                System.out.print(matrix1[mn][j]);
+                                System.out.print(" : ");
+                                System.out.print(matrix[mn++][j].replace(" ", ""));
+                                if (j != str.length() - 1) {
+                                    System.out.print(" , ");
+                                }
+                            }
+                            System.out.println();
                         }
+                        break;
                     }
-                    System.out.println();
                 }
             }
         }
-
+        if(!I){
+            System.out.println("NO");
+        }
     }
     public static Boolean testgrammer(String[][] rulee) {
         String[] c;
@@ -148,7 +146,6 @@ public class Main {
             for (int j = 0; j < np; j++) {
                 c = (rulee[j][1].split(""));
                 for (int k = 0; k < c.length; k++) {
-
                     if ((int) (c[k]).charAt(0) > 64 && (int) (c[k]).charAt(0) < 91) {
                         if (T == false){
                             return false;
@@ -159,7 +156,8 @@ public class Main {
                         else {
                             T = false;
                         }
-                    } else if ((int) (c[k]).charAt(0) > 96 && (int) (c[k]).charAt(0) < 123) {
+                    }
+                    else if ((int) (c[k]).charAt(0) > 96 && (int) (c[k]).charAt(0) < 123) {
                         if (T == false){
                             return false;
                         }
@@ -214,7 +212,7 @@ public class Main {
         }
         return r;
     }
-    private static String concat(String a, String b) //concatenates unique non-terminals
+    private static String concat(String a, String b)
     {
         int i;
         String r = a;
@@ -226,17 +224,28 @@ public class Main {
         }
         return (r);
     }
-    public static ArrayList sortt(String r){
+    public static String sortt(String r){
         ArrayList S = new ArrayList();
         ArrayList E = new ArrayList();
         for (int i =0;i<r.length();i++){
             S.add(r.charAt(i));
         }
-
         for (int i =0;i<S.size();i++){
             for (int j =0;j<S.size();j++) {
-                if (S.get(i) == S.get(j) && i!=j){
-                    S.remove(j);
+                try {
+                    if (str.length() < 100) {
+                        if (S.get(i) == S.get(j) && i != j) {
+                            S.remove(j);
+                        }
+                    }
+                    else{
+                        if (S.get(i) == S.get(j)) {
+                            S.remove(j);
+                        }
+                    }
+                }
+                catch (Exception IndexOutOfBoundsException){
+                    throw new IndexOutOfBoundsException();
                 }
             }
         }
@@ -248,7 +257,8 @@ public class Main {
         for (int i =0;i<S.size();i++){
             E.add(W[i]);
         }
-
-        return E;
+        String R = E.toString();
+        R.replace(" ","");
+        return R;
     }
 }
